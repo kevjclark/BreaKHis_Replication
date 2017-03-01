d75bcf35814c6ba17355453bd95ff2afde9e53d9
kevjclark <kevjclark@users.noreply.github.com>
2017-03-01 12:45:22 +1030

    filepath correction

--- Utilities/runData.py
+++ Utilities/runData.py
@@ -1,19 +1,19 @@
 import createfilelist as cfl
 from skimage import io
+import matplotlib.pyplot as plt
+from skimage import color
+
 
 #Change this for running on your local machine
-impath = '/home/taim/Documents/testImageV'
+impath = '/home/taim/Documents/testImages'
 imFilepath = '/home/taim/Documents/Honors/HonorsResults/imagefiles.txt'
 cfl.createflist(impath, imFilepath)
 imlist= []
 with open(imFilepath) as f:
 	for line in f:
-		filepath = impath + line
-		filepath = filepath.remove('\n')
+		filepath = impath + '/' + line
+		filepath = filepath[0:len(filepath)-1]
 		imlist.append(filepath)
 
-#for testing only
-imtest = io.imread(imlist[0], as_grey=True )
-print(imtest)
 	
 print('complete')
