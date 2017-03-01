5f87c311588f3474b1303d681eb7e246e9325966
kevjclark <kevjclark@users.noreply.github.com>
2017-03-01 11:48:12 +1030

    lbp

--- FeatureVectorScripts/LBPGeneration.py
+++ FeatureVectorScripts/LBPGeneration.py
@@ -8,14 +8,14 @@ class LBPGeneration:
 		#Sets number of points and radious for running of the LBP Algorithm, as provided in scikit-image (skimage)
 		self.points = p
 		self.radius = r
-		self.bins = np.arange(0, p+2)
-		self.range = range(0, p+1)
+		self.bins = np.arange(0, 65535+1) #65535 is the max value for 16bit values intensity images
+		self.range = range(0, 65535+1)
 
 	def start(self, filelist, iter=-1, path):
 		resultFile = open(path, 'w')
 		if iter < 0:
 			for f in filelist:
-				image = cv2.imread((path + filelist), as_grey=True )
+				image = io.imread(f, as_grey=True )
 				lbp = feature.local_binary_pattern(image, self.numPoints,self.radius, method="ror")
 				lbp = lbp.ravel()
 				(imhist, _) = np.histogram(lbp, self.bins, self.range, density = True)
